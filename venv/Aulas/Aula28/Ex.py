36fc546857b574a7bc9520b146933e648ff7d5eb
Lucas Moura <123328537+EuLucasMoura@users.noreply.github.com>
2024-05-26 18:16:11 -0300

    Enviando exercicio

--- venv/Aulas/Aula28/Ex.py
+++ venv/Aulas/Aula28/Ex.py
@@ -1,7 +1,18 @@
 nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade: "))
+idade = input("Digite sua idade: ")
 
-if nome & idade == True:
-    print('Seu nome é {}'.format(nome))
-    print('Sua idade é {}'.format(idade))
-    print('Seu nome de trás pra frente é:'.format(nome[-1]))
+if nome and idade:
+    print(f'Seu nome é {nome}.')
+    print(f'Sua idade é {idade}.')
+    print(f'Seu nome invertido é: {nome[::-1]}')
+    print(f'Seu nome tem {len(nome)} letras.')
+    print(f'A primeira letra do seu nome é {nome[0]}.')
+    print(f'A ultima letra do seu nome é {nome[-1]}.')
+
+    if ' ' in nome:
+        print("Seu nome contêm espaços")
+    else:
+        print("Seu nome não contêm espaços")
+else:
+    print("Desculpe, você deixou campos vazios")
+    
