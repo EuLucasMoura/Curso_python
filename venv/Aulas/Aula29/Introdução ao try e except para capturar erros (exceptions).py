355d70781426a73105de2f0ef247f1cd67af8c29
Lucas Moura <123328537+EuLucasMoura@users.noreply.github.com>
2024-05-27 22:20:47 -0300

    Enviando exercicios

---
+++ venv/Aulas/Aula29/Introdução ao try e except para capturar erros (exceptions).py
@@ -0,0 +1,14 @@
+"""
+Introdução ao try except
+try -> tentar executar o código
+except ->  ocorreu algum erro ao tentar executar
+"""
+
+numero_str = input("Bou dobrar o numero que você digitar: ")
+
+try: 
+    numero_float = float(numero_str)
+    print("FLOAT:", numero_float)
+    print(f"O dobro de {numero_str} é  {numero_float * 2:.2f} ")
+except: 
+    print("Isso não é um número.")
---
+++ venv/Aulas/Aula32/Ex01.py
@@ -0,0 +1,23 @@
+""" Exercício 1
+numero = int(input("Digite um número inteiro: "))
+if type(numero) == int:
+    if numero % 2 != 0:
+        print(f"O número {numero} é ímpar.")
+    else:
+        print(f"O número {numero} é par.")
+else:
+    print("O número digitado não é um inteiro.")
+"""
+
+""" Exercicio 2
+hora = int(input("Que horas são?(0-23) "))
+
+if hora < 11:
+    print("Bom dia!")
+elif 12 <= hora <= 17:
+    print("Boa tarde!")
+elif 18 <= hora <=23:
+    print("Boa noite!")
+else:
+    print("Hora inválida!")
+"""
