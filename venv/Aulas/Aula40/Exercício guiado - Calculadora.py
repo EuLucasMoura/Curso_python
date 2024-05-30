75b00dff15ee60af4a9210211dd73ebbd5493035
Lucas Moura <123328537+EuLucasMoura@users.noreply.github.com>
2024-05-30 14:28:18 -0300

    Calculadora com while

--- venv/Aulas/Aula40/Exercício guiado - Calculadora.py
+++ venv/Aulas/Aula40/Exercício guiado - Calculadora.py
@@ -1,2 +1,52 @@
+""" Minha solução
+opcao = 0
+resultado = 0
+while opcao != "5":
+    n1 = int(input("Digite um valor: "))
+    n2 = int(input("Digite outro valor: "))
+    print("="* 10 + " Calculadora " + "=" * 10)
+    print("[1] Somar")
+    print("[2] Subtrair")
+    print("[3] Dividir")
+    print("[4] Multiplicar")
+    print("[5] Sair\n")
+    print("=" * 33)
+    opcao = int(input("Escolha a operação: "))
+    if opcao == 1:
+        resultado = n1 + n2
+        print(f"A soma de {n1} e {n2} é igual a {resultado}")
+    elif opcao == 2:
+        resultado = n1 - n2
+        print(f"A subrtração de {n1} e {n2} é igual a {resultado}")
+    elif opcao == 3:
+        resultado = n1 / n2
+        print(f"A divisão entre {n1} e {n2} é igual a {resultado}")
+    elif opcao == 4:
+        resultado = n1 * n2
+        print(f"A multiplicação entre {n1} e {n2} é igual a {resultado}")
+    elif opcao == 5:
+        print("Saindo do programa...")
+        break
+print("Calculadora encerrada.")
+"""
+
 while True:
-    numero1 = int(input("Digite um número: "))
+    n1 = int(input("Digite um valor: "))
+    n2 = int(input("Digite outro valor: "))
+
+
+
+
+
+
+
+
+
+
+
+
+    sair = input("Deseja sair? Digite [s]: ").lower().startswith("s")
+    if sair is True:
+        print("Encerrando calculadora...")
+        break
+print("Calculadora encerrada.")
